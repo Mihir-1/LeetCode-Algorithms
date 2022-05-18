1f5060d570df0cf0f0deb40872bed66cb653b318
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-05-18 11:33:11 -0700

    Time: 66 ms (85.40%), Space: 17.3 MB (60.42%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,10 +1,11 @@
 class Solution(object):
     def countBits(self, n):
         ans = [0]
-        for i in range (n + 1):
-            if i:
-                t = i
-                t = t & t - 1
-                ans.append(ans[t] + 1)
+        if (not n):
+            return ans
+        for i in range (1, n + 1):
+            t = i
+            t = t & t - 1
+            ans.append(ans[t] + 1)
         return ans
         
