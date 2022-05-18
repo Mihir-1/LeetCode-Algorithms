17e06a482a286bf3c1d35abe1125249e0810f42d
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-05-18 11:34:37 -0700

    Time: 111 ms (40.80%), Space: 17.6 MB (13.95%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def countBits(self, n):
         ans = [0]
-        if (not n):
-            return ans
         for i in range (1, n + 1):
             t = i
             t = t & t - 1
