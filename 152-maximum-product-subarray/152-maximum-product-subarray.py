b12d5edcd0fd2c10a74f8a8ae5e92cfde4466791
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-05-30 23:52:54 -0700

    Time: 84 ms (45.94%), Space: 13.8 MB (79.40%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        cMin = 1
+        cMax = 1
+        res = nums[0]
+        for n in nums:
+            temp = max(cMin * n, cMax * n, n)
+            cMin = min(cMin * n, cMax * n, n)
+            cMax = temp
+            res = max(res, cMax);
+        return res
