96c5a37832850de4e2b740307d4c469b977fe2d4
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-06-10 14:24:58 -0700

    Time: 15 ms (93.69%), Space: 13.5 MB (15.95%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rob(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        else:
+            return max(self.robHelper(nums[1:]), self.robHelper(nums[:-1]))
+        
+    def robHelper(self, nums):
+        dp = [0] * (len(nums))
+        dp[0] = nums[0]
+        if(len(nums) == 1):
+            return dp[0]
+        dp[1] = max(dp[0], nums[1])
+        i = 2
+        while i < len(nums):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+            i += 1
+        return dp[len(nums) - 1]
+    
