165b32189049eaac1d10107268a9e3ee971f29bd
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-07-06 11:25:21 -0700

    Time: 1007 ms (47.04%), Space: 16.7 MB (78.73%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,25 +1,27 @@
 class Solution(object):
     def threeSum(self, nums):
-        if (len(nums) < 3):
+        if len(nums) < 3:
             return []
-        vals = dict()
         res = []
         nums.sort()
         f = 0
         while f < len(nums) - 2:
-            l = f + 1;
-            h = len(nums) - 1
-            while h - l > 0:
-                if nums[f] + nums[l] + nums[h] < 0:
+            l = f + 1
+            r = len(nums) - 1
+            tgt = 0 - nums[f]
+            print(f, l, r)
+            while (l < r):
+                while l < r and nums[l] + nums[r] > tgt:
+                    r -= 1
+                while l < r and nums[l] + nums[r] < tgt:
                     l += 1
-                elif nums[f] + nums[l] + nums[h] > 0:
-                    h -= 1
-                else:
-                    res.append([nums[f], nums[l], nums[h]])
-                    h -= 1
-                    while (h - l > 0 and nums[h + 1] == nums[h]):
-                        h -= 1
-            while (f != len(nums) - 2 and nums[f + 1] == nums[f]):
+                if l != r and nums[l] + nums[r] == tgt:
+                    res.append([nums[f], nums[l], nums[r]])
+                    while l < r and nums[l + 1] == nums[l]:
+                        l += 1
+                    l += 1
+            while f < len(nums) - 2 and nums[f + 1] == nums[f]:
                 f += 1
             f += 1
         return res
+                    
