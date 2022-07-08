6154437fed6c40b0dedc6812fbf7d78dd81fbc42
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-07-08 09:23:38 -0700

    Time: 210 ms (13.32%), Space: 15 MB (49.70%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def trap(self, height):
+        area = 0
+        l = 0
+        r = len(height) - 1
+        maxL = height[l]
+        maxR = height[r]
+        while l < r:
+            if maxL < maxR:
+                l += 1
+                maxL = max(maxL, height[l])
+                if height[l] < maxL:
+                    area += maxL - height[l]
+            else:
+                r -= 1
+                maxR = max(maxR, height[r])
+                if height[r] < maxR:
+                    area += maxR - height[r]
+        return area
+        
