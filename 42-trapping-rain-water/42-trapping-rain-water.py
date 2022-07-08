bbc8f7daf2228a643a2d24d0c37a44e6d91ae58a
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-07-08 09:24:17 -0700

    Time: 134 ms (55.34%), Space: 15.1 MB (49.70%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -9,12 +9,10 @@ class Solution(object):
             if maxL < maxR:
                 l += 1
                 maxL = max(maxL, height[l])
-                #if height[l] < maxL:
                 area += maxL - height[l]
             else:
                 r -= 1
                 maxR = max(maxR, height[r])
-                #if height[r] < maxR:
                 area += maxR - height[r]
         return area
         
