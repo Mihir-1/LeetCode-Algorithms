af58d4a6e3a987f0fa61ebbd283873505eaa7b3b
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-08-05 03:18:46 -0700

    Time: 41 ms (98.05%), Space: 14.4 MB (89.21%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -10,22 +10,16 @@ class Solution:
             if target < matrix[lr][lc] or target > matrix[rr][rc]:
                 break
             mPos = ((lr * cols + lc) + (rr * cols + rc)) // 2
-            #print(mPos)
             mr, mc = mPos // cols, mPos % cols
-            #print(lr, lc, mr, mc, rr, rc)
-            #print(matrix[mr][mc])
             
             if target > matrix[mr][mc]:
-                #print("c1")
                 if mc == cols - 1:
                     lr, lc = mr + 1, 0
                 else:
                     lr, lc = mr, mc + 1
             elif target < matrix[mr][mc]:
-                #print("c2")
                 rr, rc = mr, mc
             else:
                 return True
-            #print(lr, lc, mr, mc, rr, rc)
             
         return False if matrix[lr][lc] != target else True
