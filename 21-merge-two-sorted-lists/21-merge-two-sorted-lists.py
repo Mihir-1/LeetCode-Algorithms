43c2dd8229f6d64fe1544277d4962d471c42f535
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-08-10 10:26:36 -0700

    Time: 45 ms (80.79%), Space: 13.8 MB (79.56%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -20,27 +20,19 @@ class Solution:
         p = res
         
         while list1 or list2:
-            print(res)
-            print(p)
-            print(list1)
-            print(list2)
             if not list1:
-                print("c1")
                 p.next = list2
                 p = p.next
                 list2 = list2.next
             elif not list2:
-                print("c2")
                 p.next = list1
                 p = p.next
                 list1 = list1.next
             elif list1.val < list2.val:
-                print("c3")
                 p.next = list1
                 p = p.next
                 list1 = list1.next
             else:
-                print("c4")
                 p.next = list2
                 p = p.next
                 list2 = list2.next
