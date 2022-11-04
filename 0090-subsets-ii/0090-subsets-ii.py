e9fcb119768e5923d094e305e944222427e350f4
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-04 13:49:16 -0700

    Time: 74 ms (48.75%), Space: 14.2 MB (10.64%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -4,17 +4,13 @@ class Solution:
         nums.sort()
         
         def dfs(i, cur):
-            # print(i, cur)
             if i == len(nums):
                 res.append(cur)
                 return
             dfs(i + 1, cur + [nums[i]])
-            # Increment for Not include branch
             temp = nums[i]
-            # print(i, temp)
             while i < len(nums) and nums[i] == temp:
                 i += 1
-            # print(i)
             dfs(i, cur)
         
         dfs(0, [])
