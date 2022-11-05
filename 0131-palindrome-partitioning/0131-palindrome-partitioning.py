69570cf5201620f67cd9d47ddcd538af6a3db8f7
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-05 12:34:15 -0700

    Time: 1877 ms (16.00%), Space: 30.2 MB (58.93%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -2,18 +2,22 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
         def dfs(cur, i):
+            # Valid Palindrome
             valid = cur[-1] == cur[-1][::-1]
-            #print(cur, i, valid)
-            # If Valid pal: append to cur -> dfs
+
+            # Base Case if i = len(s)
             if i == len(s):
                 if valid:
                     res.append(cur)
                 return
+            
+            # If Valid pal: append to cur -> dfs
             if valid:
-                # Base Case if i = len(s)
                 dfs(cur + [s[i]], i + 1)
+            
             # Append to cur[-1] -> dfs
             dfs(cur[:-1] + [cur[-1] + s[i]], i + 1)
+            return
             
         dfs([s[0]], 1)
         return res
