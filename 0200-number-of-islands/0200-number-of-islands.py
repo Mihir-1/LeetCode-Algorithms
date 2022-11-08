2ed07c9030442d54aaf14fb2cf30da89a9b069c4
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-08 15:01:32 -0800

    Time: 438 ms (79.13%), Space: 22.5 MB (13.21%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+        visited = set()
+        q = deque()
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    #print(i, j, grid[i][j] == "1")
+                    q.append((i, j))
+                    islands += 1
+                while q:
+                    r, c = q.popleft()
+                    if (r, c) not in visited:
+                        visited.add((r, c))
+                        if 0 <= r + 1 < rows and (r + 1, c) not in visited and grid[r + 1][c] == "1": q.append((r + 1, c))
+                        if 0 <= r - 1 < rows and (r - 1, c) not in visited and grid[r - 1][c] == "1": q.append((r - 1, c))
+                        if 0 <= c + 1 < cols and (r, c + 1) not in visited and grid[r][c + 1] == "1": q.append((r, c + 1))
+                        if 0 <= c - 1 < cols and (r, c - 1) not in visited and grid[r][c - 1] == "1": q.append((r, c - 1))
+                        
+        # if grid[0][0] == "1" and (0, 0) not in visited:
+        #     #print(i, j, grid[i][j] == "1")
+        #     q.append((0, 0))
+        #     islands += 1
+        # while q:
+        #     r, c = q.popleft()
+        #     if (r, c) not in visited:
+        #         visited.add((r, c))
+        #         if 0 <= r + 1 < rows and (r + 1, c) not in visited and grid[r + 1][c] == "1": q.append((r + 1, c))
+        #         if 0 <= r - 1 < rows and (r - 1, c) not in visited and grid[r - 1][c] == "1": q.append((r - 1, c))
+        #         if 0 <= c + 1 < cols and (r, c + 1) not in visited and grid[r][c + 1] == "1": q.append((r, c + 1))
+        #         if 0 <= c - 1 < cols and (r, c - 1) not in visited and grid[r][c - 1] == "1": q.append((r, c - 1))
+        print(visited, len(visited))
+        
+        return islands
