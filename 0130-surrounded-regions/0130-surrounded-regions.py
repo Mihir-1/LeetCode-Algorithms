6f5dcec4cb0a816b4e927ad030a785d070dccc93
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-08 22:39:27 -0800

    Time: 617 ms (7.87%), Space: 23.7 MB (11.61%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -11,10 +11,8 @@ class Solution:
                     island = set()
                     q = deque()
                     q.append((r, c))
-                    # print(q)
                     while q:
                         row, col = q.popleft()
-                        # print(row, col)
                         if row == 0 or row == rows - 1 or col == 0 or col == cols - 1:
                             capture = False
                         island.add((row, col))
@@ -30,40 +28,6 @@ class Solution:
                         if 0 <= col - 1 < cols and board[row][col - 1] == 'O' and (row, col - 1) not in visited:
                             q.append((row, col - 1))
                             visited.add((row, col - 1))
-                    #print(capture, island)
                     if capture:
                         for row, col in island:
                             board[row][col] = 'X'
-
-        # # BFS
-        # capture = True
-        # island = set()
-        # q = deque()
-        # q.append((0, 1))
-        # # print(q)
-        # while q:
-        #     row, col = q.popleft()
-        #     # print(row, col)
-        #     if row == 0 or row == rows - 1 or col == 0 or col == cols - 1:
-        #         capture = False
-        #     island.add((row, col))
-        #     if 0 <= row + 1 < rows and board[row + 1][col] == 'O' and (row + 1, col) not in visited: 
-        #         q.append((row + 1, col))
-        #         visited.add((row + 1, col))
-        #     if 0 <= row - 1 < rows and board[row - 1][col] == 'O' and (row - 1, col) not in visited:
-        #         q.append((row - 1, col))
-        #         visited.add((row - 1, col))
-        #     if 0 <= col + 1 < cols and board[row][col + 1] == 'O' and (row, col + 1) not in visited:
-        #         q.append((row, col + 1))
-        #         visited.add((row, col + 1))
-        #     if 0 <= col - 1 < cols and board[row][col - 1] == 'O' and (row, col - 1) not in visited:
-        #         q.append((row, col - 1))
-        #         visited.add((row, col + 1))
-
-        # print(capture, island)
-
-"""
-Input:
-[
-["X","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"],["O","X","O","O","O","O","X","O","O","O","O","O","O","O","O","O","O","O","X","X"],["O","O","O","O","O","O","O","O","X","O","O","O","O","O","O","O","O","O","O","X"],["O","O","X","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","X","O"],["O","O","O","O","O","X","O","O","O","O","X","O","O","O","O","O","X","O","O","X"],["X","O","O","O","X","O","O","O","O","O","X","O","X","O","X","O","X","O","X","O"],["O","O","O","O","X","O","O","X","O","O","O","O","O","X","O","O","X","O","O","O"],["X","O","O","O","X","X","X","O","X","O","O","O","O","X","X","O","X","O","O","O"],["O","O","O","O","O","X","X","X","X","O","O","O","O","X","O","O","X","O","O","O"],["X","O","O","O","O","X","O","O","O","O","O","O","X","X","O","O","X","O","O","X"],["O","O","O","O","O","O","O","O","O","O","X","O","O","X","O","O","O","X","O","X"],["O","O","O","O","X","O","X","O","O","X","X","O","O","O","O","O","X","O","O","O"],["X","X","O","O","O","O","O","X","O","O","O","O","O","O","O","O","O","O","O","O"],["O","X","O","X","O","O","O","X","O","X","O","O","O","X","O","X","O","X","O","O"],["O","O","X","O","O","O","O","O","O","O","X","O","O","O","O","O","X","O","X","O"],["X","X","O","O","O","O","O","O","O","O","X","O","X","X","O","O","O","X","O","O"],["O","O","X","O","O","O","O","O","O","O","X","O","O","X","O","X","O","X","O","O"],["O","O","O","X","O","O","O","O","O","X","X","X","O","O","X","O","O","O","X","O"],["O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O"],["X","O","O","O","O","X","O","O","O","X","X","O","O","X","O","X","O","X","O","O"]]
-"""
