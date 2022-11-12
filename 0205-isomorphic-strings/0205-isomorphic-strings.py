926c3c6ba0dac85206ca4b05021e8a4f3618a270
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-12 13:18:54 -0800

    Time: 107 ms (19.56%), Space: 14.2 MB (89.20%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        replace = dict()
+        for i, c in enumerate(s):
+            r = replace.get(c, None)
+            if r and t[i] != r:
+                return False
+            if t[i] in replace.values() and replace.get(c, '') != t[i]:
+                return False
+            replace[c] = t[i]
+        return True
