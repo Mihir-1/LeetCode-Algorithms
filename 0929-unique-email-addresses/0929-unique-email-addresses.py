b0117d3d7b04046323ab885772610039c6ef90ba
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-12 13:06:39 -0800

    Time: 54 ms (94.63%), Space: 13.9 MB (97.37%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -3,9 +3,7 @@ class Solution:
         unique = set()
         for email in emails:
             email = email.split('@')
-            local = email[0].replace('.', '')
-            local = local.split('+')
-            local = local[0]
+            local = email[0].replace('.', '').split('+')[0]
             domain = email[1]
             unique.add(local + '@' + domain)
         return len(unique)
