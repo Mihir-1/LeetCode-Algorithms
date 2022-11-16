29cec05ce3970a48837a331bfbf6e284f3adbb8d
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-15 20:46:19 -0800

    Time: 55 ms (53.78%), Space: 14 MB (10.72%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
@@ -5,10 +5,13 @@ class Solution:
         for i, num in enumerate(arr):
             curMax = max(num, curMax)
             if curMax == i:
-               res +=1
+                res += 1
         return res
     
         """
-        Runtime: O(n^2)
+        Runtime: O(n)
+            - one pass
+        Memory: O(1)
+            - constant number of variables
         """
             
