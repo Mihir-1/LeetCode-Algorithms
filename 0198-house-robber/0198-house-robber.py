06ed3927a6e35accbdf992e3a25d1a7496d6ca44
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-23 11:38:06 -0800

    Time: 52 ms (67.88%), Space: 13.9 MB (66.45%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,8 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         neigh3 = neigh2 = prev = cur = 0
-        # neigh2: _*_c
-        # neigh3: *__c
+        # 3 2 p c
         for n in nums:
             temp = cur
             cur = max(neigh2 + n, prev + n)
@@ -11,5 +10,4 @@ class Solution:
             temp2 = neigh2
             neigh2 = temp1
             neigh3 = temp2
-            #print(neigh3, neigh2, prev, cur)
         return max(prev, cur)
