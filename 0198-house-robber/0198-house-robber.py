b4f82447e891521e24cb896c310cd404ebf766f6
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-23 11:39:30 -0800

    Time: 51 ms (69.59%), Space: 13.9 MB (66.45%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,8 +5,6 @@ class Solution:
         for n in nums:
             temp = cur
             cur, temp1 = max(neigh2 + n, prev + n), prev
-            prev = temp
-            temp2 = neigh2
-            neigh2 = temp1
-            neigh3 = temp2
+            prev, temp2 = temp, neigh2
+            neigh2, neigh3 = temp1, temp2
         return max(prev, cur)
