36fe7860e12db62942087fce8d59f15b02c1116e
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-23 11:21:10 -0800

    Time: 141 ms (24.31%), Space: 13.8 MB (99.84%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        prev, cur = 0, 0
+        prev = cur = 0
         for i, c in enumerate(cost):
             temp = cur
             cur = min(prev + c, cur + c)
