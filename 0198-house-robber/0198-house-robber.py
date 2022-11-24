25111f3b65381a029fd873fcc79e36a918547726
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-24 13:24:34 -0800

    Time: 35 ms (89.78%), Space: 13.8 MB (97.60%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,9 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        prev2 = prev = cur = 0
-        # 2 p c
+        prev = cur = 0
         for n in nums:
-            temp1 = cur
-            cur, temp2 = max(prev2 + n, prev + n), prev
-            prev, prev2 = temp1, temp2
-        return max(prev, cur)
+            temp = cur
+            cur = max(prev + n, cur)
+            prev = temp
+        return cur
