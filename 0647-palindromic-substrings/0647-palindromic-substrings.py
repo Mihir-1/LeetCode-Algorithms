d8b2fec85ad55d151585e3291b2eb6c763144674
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2022-11-24 18:16:47 -0800

    Time: 184 ms (80.36%), Space: 13.9 MB (38.87%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -3,19 +3,15 @@ class Solution:
         res = 0
         for i, c in enumerate(s):
             # odd
-            res += 1
-            l, r = i - 1, i + 1
-            #print(c, l, r)
+            #res -= 1
+            l = r = i
             while 0 <= l and r < len(s) and s[l] == s[r]:
                 res += 1
-                #print(l, r)
                 l -= 1
                 r += 1
-            #print(res, 'even')
-            # even
+            #even
             l, r = i, i + 1
             while 0 <= l and r < len(s) and s[l] == s[r]:
-                #print(s[l:r+1])
                 res += 1
                 l -= 1
                 r += 1
