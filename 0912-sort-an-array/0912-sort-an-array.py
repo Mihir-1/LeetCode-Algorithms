47ef478d526e1921a1c45abe8c2ad22e90337ca7
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2024-07-25 15:14:01 -0700

    Time: 1244 ms (21.57%), Space: 25.5 MB (31.08%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,16 +1,19 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         def merge(l: List[int], r: List[int]) -> List[int]:
-            # print('left', l,'\nright', r, '\n')
+            # Exit Recursion for lists smaller than 2
             if len(l) == 1 and len(r) == 1:
                 return l + r if l[0] < r[0] else r + l
             if len(l) == 0:
                 return r
             if len(r) == 0:
                 return l
+            
+            # Partition
             l = merge(l[:len(l) // 2], l[len(l) // 2:])
             r = merge(r[:len(r) // 2], r[len(r) // 2:])
-            # print('merging', l,'\n', r)
+
+            # Merge
             i, j = 0, 0
             merged = []
             while i < len(l) or j < len(r):
@@ -24,7 +27,7 @@ class Solution:
                     else:
                         merged.append(r[j])
                         j += 1
-            # print('merged', merged)
             return merged
-              
+        
+        # Initiate
         return merge(nums[:len(nums) // 2], nums[len(nums) // 2:])
