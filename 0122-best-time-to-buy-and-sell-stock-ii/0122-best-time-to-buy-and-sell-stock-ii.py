12dacf0530375d1044004f8c169a9868d0be047b
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2024-07-27 20:13:31 -0700

    Time: 50 ms (91.94%), Space: 17.8 MB (37.94%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -2,23 +2,9 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # Initialize Variables
         profit = 0
-        prevP = float('inf')
-        bought = False
         
-        # Buy/Sell at optimal points
-        for i, price in enumerate(prices):
-            nextP = prices[i + 1] if i < len(prices) - 1 else float('-inf')
-            
-            # Buy at local minima
-            if prevP >= price and price < nextP:
-                profit -= price
-                bought = True
-            
-            # Sell at local maxima
-            if prevP < price and price >= nextP and bought == True:
-                profit += price
-                bought = False
-            
-            prevP = price
-            
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+                
         return profit
