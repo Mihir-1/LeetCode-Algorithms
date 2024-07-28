8609fd8b32e5c0e2c7559b22a49344e9c71c9d2d
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2024-07-27 18:19:28 -0700

    Time: 147 ms (88.24%), Space: 21.5 MB (56.28%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.py
+++ 0554-brick-wall/0554-brick-wall.py
@@ -1,12 +1,14 @@
 class Solution:
     def leastBricks(self, wall: List[List[int]]) -> int:
-        # Initialize gaps dictionary
+        # Initialize gaps dictionary {position : number of gaps}
         gaps = {0 : 0}
         
+        # Populate dictionary
         for layer in wall:
             total = 0
             for num in layer[:-1]:
                 total += num
                 gaps[total] = 1 + gaps.get(total, 0)
-
+        
+        # Return wall height minus greatest number of gaps -> number of bricks needed to be broken
         return len(wall) - max(gaps.values())
