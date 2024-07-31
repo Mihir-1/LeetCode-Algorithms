d6d1d52e35373d7dee718785a7e757ea5d722d51
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2024-07-30 17:34:29 -0700

    Time: 230 ms (50.65%), Space: 19.5 MB (52.92%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -5,9 +5,7 @@ class Solution:
         sums = {0 : 1}
         for num in nums:
             total += num
-            #print(num, total)
             if sums.get(total - k, 0) > 0:
-                #print('sum found')
                 count += sums[total - k]
             sums[total] = 1 + sums.get(total, 0)
         return count
