4133c12dc7891ce3f783804dd403442f4fcf592d
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2024-08-09 16:54:07 -0700

    Time: 282 ms (5.03%), Space: 17.1 MB (13.66%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def validPalindromeHelper(l: int, r: int):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        
+        l, r = 0, len(s) - 1
+        while l < r:
+            print(s[l], s[r])
+            if s[l] != s[r]:
+                if s[r - 1] == s[l] or s[l + 1] == s[r]:
+                    a = validPalindromeHelper(l + 1, r)
+                    b = validPalindromeHelper(l, r - 1)
+                    return a or b
+                else:
+                    return False
+            else:
+                l += 1
+                r -= 1
+        return True
+                
+        
+'''
+    s = "baaaa"
+    
+'''
