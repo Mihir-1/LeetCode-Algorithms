af3f1b8b43cc30fed7a5bd692b26e9db2d12651d
Mihir-1 <72636547+Mihir-1@users.noreply.github.com>
2025-05-02 13:25:52 -0700

    Create 0473-matchsticks-to-square.py

---
+++ 0473-matchsticks-to-square.py
@@ -0,0 +1,30 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        if sum(matchsticks) % 4 != 0:
+            return False
+        
+        sideLength = sum(matchsticks) / 4
+        
+        lengths = [0] * 4
+        matchsticks.sort(reverse = True)
+        
+        def backtrack(i):
+            if i == len(matchsticks):
+                return True
+            
+            for side in range(4):
+                if lengths[side] + matchsticks[i] <= sideLength:
+                    lengths[side] += matchsticks[i]
+                    if backtrack(i + 1):
+                        return True
+
+                    lengths[side] -= matchsticks[i]
+        
+            return False
+
+        return backtrack(0)
+
+'''
+Time: O(4^n)
+Space: O(log n)
+'''
